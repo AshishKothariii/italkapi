4525a045a0bc4f1210a5ac3f4c9d611f46ad4e9d
AshishKotharii <notaprogamer26@gmail.com>
2023-07-12 23:32:39 +0530

    test commit

--- models/User.js
+++ models/User.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
   {
-    username: { type: String, unique: true, required: true },
-    email: { type: String, required: true },
-    password: { String, required: true },
+    username: { type: String, unique: true },
+    email: { type: String },
+    password: { String },
   },
   { timestamps: true }
 );
