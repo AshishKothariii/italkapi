8520f05c61a089cc17299ad2836e77ec2811d998
AshishKotharii <notaprogamer26@gmail.com>
2023-07-12 18:25:14 +0530

    implementing mail password username compulsory

--- models/User.js
+++ models/User.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
   {
-    username: { type: String, unique: true },
-    email: { type: String },
-    password: String,
+    username: { type: String, unique: true, required: true },
+    email: { type: String, required: true },
+    password: { String, required: true },
   },
   { timestamps: true }
 );
